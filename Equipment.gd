218dff3d10a3ae110b6facc38dbfca5bb946a962
jordanmulder <jordan.mulder92@gmail.com>
2018-08-19 09:08:17 -0600

    fix

---
+++ Equipment.gd
@@ -0,0 +1,33 @@
+extends MarginContainer
+
+var equipped = store.state.player.equipment
+var	equipment = store.equipment
+
+
+func _ready():
+	set_armor()
+
+
+func set_armor():
+	for item in equipped:
+		if(equipped[item]):
+			set_piece(store.get_item(equipped[item]))
+
+func set_piece(currItem):
+	match currItem.location:
+		'strong_hand':
+			$VBoxContainer/Armor/RightHand/StrongHand/Image.set_texture(load("res://assets/"+currItem.img))
+		'off_hand':
+			$VBoxContainer/Armor/LeftHand/OffHand/Image.set_texture(load("res://assets/"+currItem.img))
+		'head':
+			$VBoxContainer/Armor/Body/Head/Image.set_texture(load("res://assets/"+currItem.img))
+		'torso':
+			$VBoxContainer/Armor/Body/Torso/Image.set_texture(load("res://assets/"+currItem.img))
+		'legs':
+			$VBoxContainer/Armor/Body/Legs/Image.set_texture(load("res://assets/"+currItem.img))
+		'feet':
+			$VBoxContainer/Armor/Body/Feet/Image.set_texture(load("res://assets/"+currItem.img))
+
+
+func _on_Map_pressed():
+	get_tree().change_scene('res://Main.tscn')
