c0f914d4a8ca90c4e3b363099a1556955132fc8b
Jordan <jordan.mulder92@gmail.com>
2019-03-31 10:36:17 -0600

    new stuff to push

--- Drop.gd
+++ Drop.gd
@@ -1,9 +1,24 @@
 extends Area2D
 
-func start(pos):
+var PlayerState = PlayerStore.state
+var type
+
+func start(pos, t):
+	var imagePath = "res://assets/sprites/drops/" + t.image
+	var something = load(imagePath)
+	$Control/Sprite.texture = something
+	$Label.text = t.description
 	position = pos
+	type = t
 
 func _on_Drop_body_entered(body):
 	if body.name == "Player":
-		body.speed += 20
+		for stat in type.stats:
+			update_player_stats(body, stat, type.stats[stat], type.isPermanent)
 		queue_free()
+		
+func update_player_stats(player, stat, diff, perm):
+	print(stat, diff, perm)
+	player[stat] += diff
+	if perm:
+		PlayerState[stat] += diff
