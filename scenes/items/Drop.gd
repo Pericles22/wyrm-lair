ca918733cfc4186b8c923f40f6e8d7630d2b53be
Jordan <jordan.mulder92@gmail.com>
2019-04-12 12:30:37 -0600

    adding the sound effect

--- scenes/items/Drop.gd
+++ scenes/items/Drop.gd
@@ -12,10 +12,19 @@ func start(pos, t):
 
 func _on_Drop_body_entered(body):
 	if body.name == "Player":
+		var sound = AudioStreamPlayer2D.new()
+		sound.stream = load("res://assets/sounds/fx/pick-up-drop.wav")
+		sound.playing = true
+		self.add_child(sound)
 		print('here')
 		get_parent()._on_Drop_pickup(global_position)
-		queue_free()
+		$CollisionShape2D.queue_free()
+		$Lifetime.start()
 		
 func update_player_stats(player, stat, diff, perm):
 	if(player && stat && diff && perm):
 		pass
+
+
+func _on_Lifetime_timeout():
+	queue_free()
